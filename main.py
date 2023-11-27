2a1cbd7a887362188c3b1ffc970ae36bee820d36
reindert <info@codesensei.nl>
2023-11-27 10:12:49 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""main.py
+---
+Bla di bla
+"""
+
+
+def print_hi(name: str) -> None:
+    """Deze functie print hallo"""
+    print("hallo")
+    lst = [
+        1,
+        2,
+        3,
+        4,
+    ]
+    return lst.append(10)
+
+
+def my_decorator(func, x):
+    "..."
+
+    def wrapped_func(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapped_func
